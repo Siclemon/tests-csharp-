30af38176c839313499632ed747b5a121e183004
Siclemon <247134392+Siclemon@users.noreply.github.com>
2026-06-01 15:16:32 +0200

    Défilement avec buffer

--- Alphabotan.cs
+++ Alphabotan.cs
@@ -4,13 +4,50 @@ class Alphabotan
     static void Main()
     {
         string[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        Random rng = new Random();
+        int[] buffer = {100,100,100};
+        int t=0;
+        int i=0;
 
-        while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+        Console.Clear();
+
+        while (true)
         {
-            Console.WriteLine(alphabet[rng.NextInt64(0,26)]);
-            
+            i = NouvelleLettre(buffer);
+            buffer = Decale(buffer,i);
+            Console.WriteLine(alphabet[i]);
+            Thread.Sleep(Math.Max(1000-t,300));
+            t += 11;
         }
         
+    }
+
+    static private int NouvelleLettre(int[] buffer)
+    {
+        Random rng = new();
+        int i;
+        bool ok;
+
+        while (true)
+        {
+            i = (int)rng.NextInt64(0,26);
+            ok = true;
+
+            foreach (int item in buffer)
+            {
+                if (i == item) ok = false;
+            }
+
+            if (ok) return i;
+        }
+    }
+    static private int[] Decale(int[] tab, int nb)
+    {
+        for (int i = 1; i < tab.GetLength(0); i++)
+        {
+            tab[i-1] = tab[i];
+        }
+        tab[tab.GetLength(0)-1] = nb;
+
+        return tab;
     }
 }
