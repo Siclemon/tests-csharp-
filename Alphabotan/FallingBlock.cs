66fc42f89be6abbc44694ac366fdf77b5a5a70ff
Siclemon <247134392+Siclemon@users.noreply.github.com>
2026-06-01 16:58:07 +0200

    début projet

---
+++ Alphabotan/FallingBlock.cs
@@ -0,0 +1,13 @@
+public class FallingBlock
+{
+    private int x;
+    private int y;
+    private string art;
+    private string answer;
+
+    public FallingBlock(int x)
+    {
+        y = 0;
+        this.x = x;
+    }
+}
